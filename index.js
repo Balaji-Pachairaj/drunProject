c350d54622db2ca0a86652883a1e632932147022
Balaji-Pachairaj <110270910+Balaji-Pachairaj@users.noreply.github.com>
2023-02-12 09:55:26 +0530

    initiall commit
    drum project

---
+++ index.js
@@ -0,0 +1,49 @@
+const drumBtn = document.querySelectorAll(".drum")
+for (i = 0 ; i <drumBtn.length;i++ ){
+    drumBtn[i].addEventListener("click",function(){
+       playAudio(this.innerText)
+    })
+}
+
+
+document.addEventListener("keypress" , function(event){
+    playAudio(event.key)
+})
+
+
+
+function playAudio(letter){
+    switch(letter){
+        case "w":
+            let audio1 = new Audio("sounds/crash.mp3")
+            audio1.play()
+            break;
+        case "a":
+            let audio2 = new Audio("sounds/kick-bass.mp3")
+            audio2.play()
+            break;
+        case "s":
+            let audio3 = new Audio("sounds/snare.mp3")
+            audio3.play()
+            break;
+        case "d":
+            let audio4 = new Audio("sounds/tom-1.mp3")
+            audio4.play()
+            break;
+        case "j":
+            let audio5 = new Audio("sounds/tom-2.mp3")
+            audio5.play()
+            break;
+        case "k":
+            let audio6 = new Audio("sounds/tom-3.mp3")
+            audio6.play()
+            break;
+        case "l":
+            let audio7 = new Audio("sounds/tom-4.mp3")
+            audio7.play()
+            break;
+        default:
+            console.log(letter)
+       }
+}
+
